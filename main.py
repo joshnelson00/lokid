d60db7ba6b419b5967dbace183590e43497e39fb
Joshua Nelson <joshuaknelson1202@icloud.com>
2026-03-26 09:15:46 -0700

    added backend for servicing

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from chaos import get_all_nodes, get_all_pods
 from kubernetes import client, config
-import random
-# Configs can be set in Configuration class directly or using helper utility
 
-def get_all_pods(api) -> list:
-    response = api.list_pod_for_all_namespaces(watch=False)
-    return response.items
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+config.load_kube_config()
+api = client.CoreV1Api()
 
-def delete_random_pod(api, namespace) -> str:
-    response = api.list_pod_for_all_namespaces(watch=False)
-    pods = response.items
-    pod_to_delete = random.choice(pods)
-    api.delete_namespaced_pod(name=pod_to_delete.metadata.name, namespace=namespace)
-    return pod_to_delete.metadata.name
-
-def main():
-    config.load_kube_config()
-    api = client.CoreV1Api()
-
+@app.get("/")
+async def root(request: Request):
+    nodes = get_all_nodes(api)
     pods = get_all_pods(api)
-    # Listing All Pods
-    print("IP\t\tNAMESPACE\tNAME")
-    print("--------------------------------------------------")
-    for i in pods:
-        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-
-    # Deleting Random Pod
-    namespace = "default"
-    deleted_pod_name = delete_random_pod(api, namespace=namespace)
-    print(f"Deleted Pod: {deleted_pod_name}")
-
-if __name__ == "__main__":
-    main()
-
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"name": "Josh", "nodes": nodes, "pods": pods}
+    ) 
